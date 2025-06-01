b3b1844c219f59343f3ab2ce2882f9aeb0b22dca
Алексей Коновалов <lex-sorokin@yandex.ru>
2025-06-01 16:58:49 +0500

    fixed processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,14 @@
-_oper = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-         ]
+from typing import Union
 
-from src.widget import get_date
-def filter_by_state(operations: list[dict], state: str = "EXECUTED") -> list[dict]:
+_oper = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+
+
+def filter_by_state(operations: Union[list], state: str = "EXECUTED") -> list:
     """
     Фильтрует операции по статусу
     :param operations: список словарей (транзакций)
@@ -14,15 +17,14 @@ def filter_by_state(operations: list[dict], state: str = "EXECUTED") -> list[dic
     """
     result = []
     for operation in operations:
-        if operation['state'] == state:
+        if operation["state"] == state:
             result.append(operation)
     return result
 
 
-def sort_by_date(operations: list[dict], reverse=True) -> list[dict]:
+def sort_by_date(operations: Union[list], sorting: bool = True) -> list:
     """Функция, которая сортирует списки словарей по дате."""
-    for operation in operations:
-        result = sorted(operations, key=lambda operation : get_date(operation['date']))
+    result = sorted(operations, key=lambda operation: operation.get("date"), reverse=sorting)
     return result
 
 
