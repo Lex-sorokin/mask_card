e298590393169d14c33587e7686f0927fe8b7385
Алексей Коновалов <lex-sorokin@yandex.ru>
2025-06-30 01:00:46 +0500

    Создан модуль utils.py, external_api.py, test_utils.py, test_external_api.py, поправлен файл README.md

---
+++ src/utils.py
@@ -0,0 +1,28 @@
+import json
+
+from src.external_api import converter
+
+
+def open_json_file(path: str) -> list[dict]:
+    """Функция, которая преобразует JSON-объект в Pyton-объект"""
+    try:
+        with open(path, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except json.JSONDecodeError:
+        return []
+    except Exception:
+        return []
+
+def transaction_amount(transaction: dict) -> float:
+    """Функция, которая конвертирует валюту в рубли"""
+    # currency = transaction['operationAmount']["currency"]["code"]
+    # amount = transaction['operationAmount']["amount"]
+    try:
+        if transaction['operationAmount']["currency"]["code"] == "RUB":
+            return float(transaction['operationAmount']["amount"])
+        else:
+            currency = transaction['operationAmount']["currency"]["code"]
+            amount = transaction['operationAmount']["amount"]
+            return float(converter(currency, amount))
+    except (KeyError, ValueError):
+        return 0.0
